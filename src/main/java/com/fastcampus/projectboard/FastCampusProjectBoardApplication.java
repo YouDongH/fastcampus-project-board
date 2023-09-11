8b9d7b6185a5f7f6c47ce7e98d2b5b0c4c26cf19
YouDongH <ydh312@naver.com>
2023-09-12 08:25:25 +0900

    #16 - Thymeleaf3를 사용하기 위한 설정

--- src/main/java/com/fastcampus/projectboard/FastCampusProjectBoardApplication.java
+++ src/main/java/com/fastcampus/projectboard/FastCampusProjectBoardApplication.java
@@ -2,8 +2,9 @@ package com.fastcampus.projectboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
-//@ConfigurationPropertiesScan
+@ConfigurationPropertiesScan	// ConfigurationProperties 직접 만들었을 때는 이 애너테이션 필요
 @SpringBootApplication
 public class FastCampusProjectBoardApplication {
 
---
+++ src/main/java/com/fastcampus/projectboard/config/ThymeleafConfig.java
@@ -0,0 +1,37 @@
+package com.fastcampus.projectboard.config;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+
+@Configuration
+public class ThymeleafConfig {
+
+    @Bean
+    public SpringResourceTemplateResolver thymeleafTemplateResolver(
+            SpringResourceTemplateResolver defaultTemplateResolver,
+            Thymeleaf3Properties thymeleaf3Properties
+    ) {
+        defaultTemplateResolver.setUseDecoupledLogic(true);
+
+        return defaultTemplateResolver;
+    }
+
+
+    @RequiredArgsConstructor
+    @Getter
+    @ConstructorBinding
+    @ConfigurationProperties("spring.thymeleaf3")
+    public static class Thymeleaf3Properties {
+        /**
+         * Use Thymeleaf 3 Decoupled Logic
+         */
+        // application 파일에서 thymeleaf3: decoupled-logic: true 추가
+//        private final boolean decoupledLogic; // 이부분 에러 해결안되서 그냥 true넣음
+    }
+
+}
