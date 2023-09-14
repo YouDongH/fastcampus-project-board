350e1b0a07bb00cebc62d0f7aa5256df0969544c
YouDongH <ydh312@naver.com>
2023-09-14 16:07:49 +0900

    #18- 그외변경추가
    클래스 이름변경

---
+++ src/main/java/com/fastcampus/projectboard/FastcampusProjectBoardApplication.java
@@ -0,0 +1,15 @@
+package com.fastcampus.projectboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+@ConfigurationPropertiesScan	// ConfigurationProperties 직접 만들었을 때는 이 애너테이션 필요
+@SpringBootApplication
+public class FastcampusProjectBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastcampusProjectBoardApplication.class, args);
+	}
+
+}
