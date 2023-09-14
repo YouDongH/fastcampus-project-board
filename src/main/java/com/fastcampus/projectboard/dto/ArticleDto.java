dedf4df76dbb46d52407c4bc73e23391505f98b9
YouDongH <ydh312@naver.com>
2023-09-14 15:59:18 +0900

    #18-User 도메인추가
    영향 끼치는 변경추가

--- src/main/java/com/fastcampus/projectboard/dto/ArticleDto.java
+++ src/main/java/com/fastcampus/projectboard/dto/ArticleDto.java
@@ -1,5 +1,7 @@
 package com.fastcampus.projectboard.dto;
 
+import com.fastcampus.projectboard.domain.Article;
+
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
@@ -7,13 +9,44 @@ import java.time.LocalDateTime;
  * DTO for {@link com.fastcampus.projectboard.domain.Article}
  */
 public record ArticleDto(
-        LocalDateTime createdAt,
-        String createdBy,
+        Long id,
+        UserAccountDto userAccountDto,
         String title,
         String content,
-        String hashtag
-) implements Serializable {
-    public static ArticleDto of(LocalDateTime createdAt, String createdBy, String title, String content, String hashtag) {
-        return new ArticleDto(createdAt, createdBy, title, content, hashtag);
+        String hashtag,
+        LocalDateTime createdAt,
+        String createdBy,
+        LocalDateTime modifiedAt,
+        String modifiedBy
+) {
+    public static ArticleDto of(Long id, UserAccountDto userAccountDto, String title, String content, String hashtag, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
+    return new ArticleDto(id, userAccountDto, title, content, hashtag, createdAt, createdBy, modifiedAt, modifiedBy);
+}
+
+    // 이렇게 작성을하면 Domain코드는 dto의 존재를 몰라도 됨 -> Domain코드가 영향안받음 OSIV
+
+    // entity로부터 dto생성
+//    public static ArticleDto from(Article entity) {
+//        return new ArticleDto(
+//                entity.getId(),
+//                UserAccountDto.from(entity.getUserAccount()),
+//                entity.getTitle(),
+//                entity.getContent(),
+//                entity.getHashtag(),
+//                entity.getCreatedAt(),
+//                entity.getCreatedBy(),
+//                entity.getModifiedAt(),
+//                entity.getModifiedBy()
+//        );
+//    }
+
+    // dto로부터 엔티티 생성
+    public Article toEntity() {
+        return Article.of(
+                userAccountDto.toEntity(),
+                title,
+                content,
+                hashtag
+        );
     }
 }
