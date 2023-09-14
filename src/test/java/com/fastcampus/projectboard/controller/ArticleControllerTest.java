de04b9bdc6d37ba3290562caa51bb834adfc94e5
YouDongH <ydh312@naver.com>
2023-09-15 05:03:09 +0900

    #18-1 게시판 컨트롤러 구현
    기본조회화면 구현

--- src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
@@ -1,16 +1,29 @@
 package com.fastcampus.projectboard.controller;
 
 import com.fastcampus.projectboard.config.SecurityConfig;
+import com.fastcampus.projectboard.dto.ArticleWithCommentsDto;
+import com.fastcampus.projectboard.dto.UserAccountDto;
+import com.fastcampus.projectboard.service.ArticleService;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -21,39 +34,47 @@ class ArticleControllerTest {
 
     private final MockMvc mvc;
 
+    @MockBean // mokito의 mock같음
+    private ArticleService articleService;
+
     public ArticleControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
     }
 
-    @DisplayName("view_get_articles board page")
+    @DisplayName("게시글 리스트 페이지 - 정상호출")
     @Test
     public void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
         //given
-
+        given(articleService.searchArticles(eq(null),eq(null),any(Pageable.class))).willReturn(Page.empty());
         //When&Then
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))    // 호환되는 타입까지
                 .andExpect(view().name("articles/index"))
                 .andExpect(model().attributeExists("articles"));
+        
+        then(articleService).should().searchArticles(eq(null),eq(null),any(Pageable.class));
     }
 
-    @DisplayName("view_get_articles detail page")
+    @DisplayName("게시글 상세페이지 - 정상호출")
     @Test
     public void givenNothing_whenRequestingArticleView_thenReturnsArticleView() throws Exception {
         //given
-
+        Long articleId=1L;
+        given(articleService.getArticle(articleId)).willReturn(createArticleWithCommentsDto());
         //When&Then
-        mvc.perform(get("/articles/1"))
+        mvc.perform(get("/articles/"+articleId))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/detail"))
                 .andExpect(model().attributeExists("article"))
                 .andExpect(model().attributeExists("articleComments"));
+
+        then(articleService).should().getArticle(articleId);
     }
 
     @Disabled
-    @DisplayName("view_get_articles search page")
+    @DisplayName("게시글 검색 전용페이지-정상호출")
     @Test
     public void givenNothing_whenRequestingArticleSearchView_thenReturnsArticleSearchView() throws Exception {
         //given
@@ -66,7 +87,7 @@ class ArticleControllerTest {
     }
 
     @Disabled
-    @DisplayName("view_get_articles hashtag search page")
+    @DisplayName("해시태그 검색페이지-정상호출")
     @Test
     public void givenNothing_whenRequestingArticleHashtagSearchView_thenReturnsArticleHashtagSearchView() throws Exception {
         //given
@@ -76,5 +97,36 @@ class ArticleControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(model().attributeExists("article/search-hashtag"));
+    }
+
+    private ArticleWithCommentsDto createArticleWithCommentsDto(){
+        return ArticleWithCommentsDto.of(
+                1L,
+                createUserAccountDto(),
+                Set.of(),
+                "title",
+                "content",
+                "#hashtag",
+                LocalDateTime.now(),
+                "title",
+                LocalDateTime.now(),
+                "title"
+
+        );
+    }
+    private UserAccountDto createUserAccountDto(){
+        return UserAccountDto.of(
+                1L,
+                "dong",
+                "pw",
+                "email",
+                "dong",
+                "memo",
+                LocalDateTime.now(),
+                "dong",
+                LocalDateTime.now(),
+                "dong"
+
+        );
     }
 }
