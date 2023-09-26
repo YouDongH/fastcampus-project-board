85c2ac633b85f6beb220db522538a2336f89ff3c
YouDongH <ydh312@naver.com>
2023-09-26 20:27:02 +0900

    #36-패스워드 정보수정
    {noop} : 인코딩 안해서 저장하겠다

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,13 @@
 -- 테스트 계정
 -- TODO: 테스트용이지만 비밀번호가 노출된 데이터 세팅. 개선하는 것이 좋을 지 고민해 보자.
  insert into user_account (user_id, user_password, nickname, email, memo, created_at, created_by, modified_at, modified_by) values
-     ('dong', '1234', 'dong', 'dong@mail.com', 'I am Dong.', now(), 'dong', now(), 'dong')
+     ('dong', '{noop}1234', 'dong', 'dong@mail.com', 'I am Dong.', now(), 'dong', now(), 'dong')
  ;
  insert into user_account (user_id, user_password, nickname, email, memo, created_at, created_by, modified_at, modified_by) values
-     ('dong2', '1234', 'Dong2', 'dong2@mail.com', 'I am Dong2.', now(), 'dong2', now(), 'dong2')
+     ('dong2', '{noop}1234', 'Dong2', 'dong2@mail.com', 'I am Dong2.', now(), 'dong2', now(), 'dong2')
  ;
  insert into user_account (user_id, user_password, nickname, email, memo, created_at, created_by, modified_at, modified_by) values
-     ('don3', '1234', 'Dong2', 'dong3@mail.com', 'I am Dong3.', now(), 'dong3', now(), 'dong3')
+     ('don3', '{noop}1234', 'Dong2', 'dong3@mail.com', 'I am Dong3.', now(), 'dong3', now(), 'dong3')
 ;
 -- 123 게시글
 insert into article (user_id, title, content, hashtag, created_by, modified_by, created_at, modified_at) values
